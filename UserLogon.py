c9e19e0afa5c5cbd7744fdfc814c4fe67454679b
pxs406 <pxs406@CPSC222.myguest.virtualbox.org>
2024-03-17 22:30:58 -0400

    UserLogon.py

--- UserLogon.py
+++ UserLogon.py
@@ -1,37 +1,43 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, redirect
 import pwd
 import grp
 app = Flask(__name__)
 
-USERNAME = "test"
-PASSWORD = "abcABC123"
+USERNAME = 'test'
+PASSWORD = 'abcABC123'
 
 def authenticate():
 	auth = request.authorization
-	if not auth or auth.username != USERNAME or auth.password!= PASSWORD:
+	if not auth or auth.username != USERNAME or auth.password != PASSWORD:
 		return False
 	return True
 
-@app.route('/api/users', methods = ['GET', 'POST'])
+@app.route('/api/users', methods = ['POST'])
 def users(): 
+	if rquest.method != 'POST':
+		return jsonify({'error': 'Method Not Allowed'}), 405
 	if not authenticate():
-		return jsonify({'error': 'Unauthorized acess1111'}), 401
-	
-	user.info = {}
+		return jsonify({'error': 'Unauthorized access1111'}), 401
+	user_info = {}
 	for user in pwd.getpwall():
 		users_info[str(user.pw_uid)] = user.pw_name
 	return jsonify(users_info)
 
-@app.route('/api/groups', methods = ['GET', 'POST'])
+@app.route('/api/groups', methods = ['POST'])
 def groups(): 
+	if request.method != 'POST':
+		return jsonify({'error': 'Method Not Allowed'}), 405
 	if not authenticate():
-		return jsonify({'error': 'Unauthorized acess2222'}), 401
-	
-	groups.info = {}
-	for group in grp.getpwall():
+		return jsonify({'error': 'Unauthorized access2222'}), 401
+	groups_info = {}
+	for group in grp.getgrall():
 		groups_info[str(group.gr_gid)] = group.gr_name
 	return jsonify(groups_info)
-	
-if__name__ == '__main__':
-	app.run(host'0.0.0.0', port = 3000)
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port = 3000)
+@app.route('/')
+def index():
+	return send file('index.html')
+if_name_=='_main_':
+	app.run(host='10.0.2.15',port=3000)
 
